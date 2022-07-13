f93e285a521bf207ed24bfa15e9ab7e0d6dc1e49
Lawlesh2022 <106768533+Lawlesh2022@users.noreply.github.com>
2022-07-13 23:35:05 +0530

    Add files via upload

--- auth.js
+++ auth.js
@@ -1,19 +1,22 @@
 class auth{
     constructor(){
         document.querySelector("body").style.display = "none"
-        const auth = localStorage.getItem("auth")
+        const auth = localStorage.getItem("auth");
         this.validateAuth(auth)
     }
     validateAuth(auth){
       if(auth!=1){
-        window.location.replace("/")
+        window.location.replace("/");
       }
       else{
         document.querySelector("body").style.display = "block"
       }
     }
     logout() {
-        localStorage.removeItem("auth")
-        window.location.replace("/")
+        localStorage.removeItem("auth");
+        window.location.replace("/");
     }
+}
+function getInfo(){
+  document.write("Logout Successfully");
 }
--- index.js
+++ index.js
@@ -1,14 +1,76 @@
-function getInfo(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+class Login{
+    constructor(form, fields){
+        this.form = form
+        this.fields = fields
+        this.validateonSubmit()
+    }
+    validateonSubmit(){
+        let self = this
+
+        this.form.addEventListner("submit", (e)=>{
+            e.preventDefault()
+             var error = 0
+            self.fields.forEach((field)=>{
+                const input = document.querySelector(`#${field}`)
+                if(self.validateFields(input) == false){
+                    error++
+                }
+                if(error == 0){
+                    localStorage.setItem("auth", 1)
+                    this.form.submit()
+                }
+            })
+        })
+    }
+    validateFields(field){
+        if(field.value.trim()==""){
+            this.setStatus(field, 
+                `${field.previousElementSibling.innerText} cannot be blank`,
+                 "error"
+            )
+            return false
+        }else{
+            if(field.type == "password"){
+                if(field.value < 8){
+                    this.setStatus(field, 
+                        `${field.previousElementSibling.innerText} must be atleast 8 characters`,
+                         "error"
+                    )
+                    return false
+                }
+                else{
+                    this.setStatus(field, null, "succes")
+                    return true
+                }
+            }else{
+                this.setStatus(field, null, "succes")
+                return true
+            }
+
+        }
+
+    }
+    setStatus(field, message, status){
+        const errorMessage = field.parentElement.querySelector(".error-message");
+        if(status == "success"){
+            if(errorMessage){
+                errorMessage.innerText = ""
+            }
+            field.classlist.remove("input-error")
+        }
+        
+        if(status =="error"){
+            errorMessage.innerText = message
+            field.classList.add("input-error")
+        }
+    }
     
+}
+function logIn(){
+    var username = document.getElementById("usernsme").value;
+    var username = document.getElementById("usernsme").value;
     if(username == "admin" && password == "12345"){
-        document.write("Login Successful");
-           
+        documents.write("Login Successfully");
     }
-    else{
-        document.write("Login Failled");
-            
-    }
-    
+    document.write("Login Failed");    
 }
